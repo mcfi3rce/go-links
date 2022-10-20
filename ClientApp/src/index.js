bdf4875afbd38889dc1c4ec143f5e3d7f7851251
mcfi3rce <aomcpherson@gmail.com>
2022-10-20 00:02:31 -0600

    fix: now it doesn't kick you out on refreshs

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,40 +1,42 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 import { getConfig } from "./config";
-import {Auth0Provider} from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import history from "./utils/history";
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 const config = getConfig();
 
 const onRedirectCallback = (appState) => {
-    history.push(
-        appState && appState.returnTo ? appState.returnTo : window.location.pathname
-    );
+  history.push(
+    appState && appState.returnTo ? appState.returnTo : window.location.pathname
+  );
 };
 
 const providerConfig = {
-    domain: config.domain,
-    clientId: config.clientId,
-    ...(config.audience ? { audience: config.audience } : null),
-    redirectUri: window.location.origin,
-    onRedirectCallback,
+  domain: config.domain,
+  clientId: config.clientId,
+  ...(config.audience ? { audience: config.audience } : null),
+  redirectUri: window.location.origin,
+  onRedirectCallback,
+  useRefreshTokens: true,
+  cacheLocation: "localstorage",
 };
 
 root.render(
-    <Auth0Provider {...providerConfig}>
-        <BrowserRouter basename={baseUrl}>
-            <App />
-        </BrowserRouter>
-    </Auth0Provider>
-        );
+  <Auth0Provider {...providerConfig}>
+    <BrowserRouter basename={baseUrl}>
+      <App />
+    </BrowserRouter>
+  </Auth0Provider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
